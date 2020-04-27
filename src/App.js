539e447bacac777bb5d1b729f92364b50bb12c00
6035512006 <54774484+6035512006@users.noreply.github.com>
2020-04-27 11:27:02 -0700

    ส่วน Header

--- src/App.js
+++ src/App.js
@@ -1,49 +1,26 @@
 import React from 'react'
-import styled from 'styled-components'
+import Home from './components/Home'
+import './App.css'
 
-import Nav from './Nav'
-import Modal from './Modal'
 
-const Div = styled.div`
-  margin: 0;
-  padding: 0;
-  color: grey;
-  background: white;
-`
-
-const InnerDiv = styled.div`
-  margin: 0 auto;
-  width: 80%;
-  text-align: center;
-`
 
 class App extends React.Component {
-  state = {
-    signupOpen: false,
-    signinOpen: false
-  }
-
-  handleSignup = () => this.setState({ signupOpen: true })
-
-  handleSignin = () => this.setState({ signinOpen: true })
-
-  handleClose = () => this.setState({ signupOpen: false, signinOpen: false })
 
   render() {
     return (
-      <Div>
-        <Nav
-          handleSignup={this.handleSignup}
-          handleSignin={this.handleSignin}
-        />
-        <InnerDiv>
-          <h1>Welcome to Gundam M Store</h1>
-          {(this.state.signupOpen || this.state.signinOpen) && (
-            <Modal state={this.state} handleClose={this.handleClose} />
-          )}
-        </InnerDiv>
-      </Div>
+      <div className="App">
+        <Home />
+      </div>
     )
   }
+
+
+
+
+
 }
+
+
+
+
 export default App
